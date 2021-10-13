eca17bc5d574f1b13a120500af52f679d3c55fc1
J-BAEM <43054449+JongBeomBaek@users.noreply.github.com>
2021-10-14 01:39:43 +0900

    Update builder.py

--- moco/builder.py
+++ moco/builder.py
@@ -11,9 +11,9 @@ class MoCo(nn.Module):
     def __init__(self, base_encoder, dim=128, K=65536, m=0.999, T=0.07, mlp=False):
         """
         dim: feature dimension (default: 128)
-        K: queue size; number of negative keys (default: 65536)
-        m: moco momentum of updating key encoder (default: 0.999)
-        T: softmax temperature (default: 0.07)
+        K: queue size; number of negative keys (default: 65536)     
+        m: moco momentum of updating key encoder (default: 0.999)   
+        T: softmax temperature (default: 0.07)                      contrastive loss에 사용
         """
         super(MoCo, self).__init__()
 
@@ -23,23 +23,25 @@ class MoCo(nn.Module):
 
         # create the encoders
         # num_classes is the output fc dimension
-        self.encoder_q = base_encoder(num_classes=dim)
+        self.encoder_q = base_encoder(num_classes=dim)  # 출력할 클래스에 맞춰서 fc마지막 출력 channel이 변경됨 >> adjust fc layer output channel about the number of class
         self.encoder_k = base_encoder(num_classes=dim)
 
-        if mlp:  # hack: brute-force replacement
+        if mlp:  # hack: brute-force replacement            # moco에서 추가적으로 사용하는 mlp layer [projection head]  [기존 dim_mlp > # of class  // 변경후 dim_mlp > dim_mlp > ReLU  > # of class]
             dim_mlp = self.encoder_q.fc.weight.shape[1]
             self.encoder_q.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
             self.encoder_k.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_k.fc)
 
+        # key 에 있는 weight를 q를 기준으로 copy 학습을 하지않고 _momentum_update_key_encoder() 함수를 통해 업데이트하기 때문에
+        # require_grad = False
         for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
             param_k.data.copy_(param_q.data)  # initialize
             param_k.requires_grad = False  # not update by gradient
 
         # create the queue
-        self.register_buffer("queue", torch.randn(dim, K))
-        self.queue = nn.functional.normalize(self.queue, dim=0)
+        self.register_buffer("queue", torch.randn(dim, K))              #optimizer가 업데이트 하지않고 값만 존재 [state_dict() 확인 가능, GPU연산 가능]
+        self.queue = nn.functional.normalize(self.queue, dim=0)         #link: https://discuss.pytorch.org/t/what-is-the-difference-between-register-buffer-and-register-parameter-of-nn-module/32723/6
 
-        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long)) #point
 
     @torch.no_grad()
     def _momentum_update_key_encoder(self):
@@ -47,10 +49,10 @@ class MoCo(nn.Module):
         Momentum update of the key encoder
         """
         for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
-            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)                 #현재값 * m(0.999) + 새로운 값(q) * (1-m) 
 
     @torch.no_grad()
-    def _dequeue_and_enqueue(self, keys):
+    def _dequeue_and_enqueue(self, keys):           #cycle queue
         # gather keys before updating queue
         keys = concat_all_gather(keys)
 
@@ -123,7 +125,7 @@ class MoCo(nn.Module):
 
         # compute query features
         q = self.encoder_q(im_q)  # queries: NxC
-        q = nn.functional.normalize(q, dim=1)
+        q = nn.functional.normalize(q, dim=1)       #channel 축으로 normalization
 
         # compute key features
         with torch.no_grad():  # no gradient to keys
@@ -141,7 +143,7 @@ class MoCo(nn.Module):
         # compute logits
         # Einstein sum is more intuitive
         # positive logits: Nx1
-        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
+        l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)      
         # negative logits: NxK
         l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
 
@@ -162,14 +164,14 @@ class MoCo(nn.Module):
 
 # utils
 @torch.no_grad()
-def concat_all_gather(tensor):
+def concat_all_gather(tensor):      #분산된 tensor concat 
     """
     Performs all_gather operation on the provided tensors.
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
-    tensors_gather = [torch.ones_like(tensor)
+    tensors_gather = [torch.ones_like(tensor)                               # 분산된 rank 개수만큼 l생성 
         for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)    # boradcast 
 
-    output = torch.cat(tensors_gather, dim=0)
+    output = torch.cat(tensors_gather, dim=0)                               # concat 
     return output
